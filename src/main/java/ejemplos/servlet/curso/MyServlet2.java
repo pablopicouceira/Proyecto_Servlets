7f1a6999db1c24bf789194246507826f840c4b6c
Pablo Pico Uceira <pablo.pico.uceira@gmail.com>
2023-12-15 11:43:37 +0100

    Modificamos código para recibir nombre del form

--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -21,7 +21,7 @@ public class MyServlet2 extends HttpServlet {
 		out.println("<html>");
 		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+		out.println("<h1>Bienvenido "+request.getParameter("fname")+"</h1>");
 	}
 
 }
